eb41c6a7b5a0dff38788a0b3cfffded53de95664
Abenezer Daniel <aabenezzer@gmail.com>
2025-03-18 15:16:38 +0300

    build a function to manage loading state

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
-const API = "NMFMV24NU7LYE37GHNHVZFKC3";
+const form = document.querySelector('form');
+const isLoading = document.querySelector('.loading');
+const wrapper = document.querySelector('.wrapper');
+
+const address = document.querySelector('.address');
+const timeZone = document.querySelector('.time-zone');
+const description = document.querySelector('.description');
+
 
 async function getData(city = "london") {
+    const API = "NMFMV24NU7LYE37GHNHVZFKC3";
   const url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}?key=${API}`;
   const res = await fetch(url, { mode: "cors" });
   const data = await res.json();
@@ -14,6 +22,14 @@ function getSpecificData(data) {
     timezone: data.timezone,
     description: data.description,
   };
+}
+
+function isLoading(val) {
+    if(val) {
+        isLoading.style.display = 'block';
+    } else {
+        isLoading.style.display = 'none';
+    }
 }
 
 getData('Adama');
