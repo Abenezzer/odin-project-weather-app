e6570c90908880ef22684910267961af2b950ffb
Abenezer Daniel <aabenezzer@gmail.com>
2025-03-18 14:35:24 +0300

    Build a function to get only specific data from a fetch result

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
-
 const API = "NMFMV24NU7LYE37GHNHVZFKC3";
 
-async function getData(city='london') {
-    const url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}?key=${API}`;
-    const res = await  fetch(url, {mode: "cors"});
-    const data = await res.json();
-    console.log(data);
+async function getData(city = "london") {
+  const url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}?key=${API}`;
+  const res = await fetch(url, { mode: "cors" });
+  const data = await res.json();
+  return getSpecificData(data);
 }
 
-getData('Adama');
+getData("Adama");
+
+function getSpecificData(data) {
+  return {
+    address: data.resolvedAddress,
+    timeZone: data.timeZone,
+    description: data.description,
+  };
+}
